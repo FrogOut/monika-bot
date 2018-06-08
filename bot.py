937df9293783d2cd91fd9221fcbf4ea7d6fbf571
FrogOut <40074583+FrogOut@users.noreply.github.com>
2018-06-09 00:02:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,4 +18,4 @@ async def on_message(message):
 	if message.content.startswith('m!help'):
 		await client.send_message(message.channel, "HELP")
 
-client.run("NDU0NzUzOTg3MDUzNDg2MDkw.DfyCLQ.smx6ng_f3_C4PM9tHrxeCFoFrHY");
+client.run("BOT_TOKEN);
