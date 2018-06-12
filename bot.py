9367d253920a82b7f06efeb032d3650814c844ce
FrogOut <40074583+FrogOut@users.noreply.github.com>
2018-06-12 18:03:14 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,8 +26,9 @@ async def on_message(message):
 	if message.content.startswith('night'):
 		await client.send_message(message.channel, "Good night :full_moon:")
 	if message.content.startswith('m!triggers'):
-		await client.send_message(message.channel, """This bot is still in the makings, 
-Currently availlable 'trigger words': 
-bing, morning, afternoon, evening, night""")
+		await client.send_message(message.channel, "This bot is still in the makings,
+					  
+ Currently availlable 'trigger words': 
+ bing, morning, afternoon, evening, night")
 		
 client.run("NDU0NzUzOTg3MDUzNDg2MDkw.DfyPlA.Mveayqzgk_h6KU_goTxNJaCOeWQ");
